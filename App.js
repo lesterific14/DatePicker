8f0268e32dadddfaf0d0d649210a7f61b77ab2e6
lesterific14 <147916616+lesterific14@users.noreply.github.com>
2024-11-29 13:05:40 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import MonthPicker from './MonthPicker';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <MonthPicker/>
+    </SafeAreaView>
+  );
+
+};
+export default App;
